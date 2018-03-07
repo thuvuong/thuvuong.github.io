e8647b72c83ef5e6d6979947880f8a05fb978c0f
thuvuong <thuv@uw.edu>
2018-03-07 11:08:42 -0800

    add assignment2 folder
    good

---
+++ assignment2/main.js
@@ -0,0 +1,264 @@
+
+// GameBoard code below
+
+function distance(a, b) {
+    var dx = a.x - b.x;
+    var dy = a.y - b.y;
+    return Math.sqrt(dx * dx + dy * dy);
+}
+
+function Circle(game) {
+    this.player = 1;
+    this.radius = 20;
+    this.visualRadius = 500;
+    this.colors = ["Red", "Green", "Blue", "White"];
+    this.setNotIt();
+    Entity.call(this, game, this.radius + Math.random() * (800 - this.radius * 2), this.radius + Math.random() * (800 - this.radius * 2));
+
+    this.velocity = { x: Math.random() * 1000, y: Math.random() * 1000};
+    var speed = Math.sqrt(this.velocity.x * this.velocity.x + this.velocity.y * this.velocity.y);
+    if (speed > maxSpeed) {
+        var ratio = maxSpeed / speed;
+        this.velocity.x *= ratio;
+        this.velocity.y *= ratio;
+    }
+};
+
+Circle.prototype = new Entity();
+Circle.prototype.constructor = Circle;
+
+Circle.prototype.setIt = function () {
+    this.it = true;
+    this.color = 0;
+    this.visualRadius = 500;
+};
+
+Circle.prototype.setNotIt = function () {
+    this.it = false;
+    this.color = 3;
+    this.visualRadius = 200;
+};
+
+Circle.prototype.collide = function (other) {
+    return distance(this, other) < this.radius + other.radius;
+};
+
+// return true if the rectangle and circle are colliding
+Circle.prototype.rectCircleColliding = function (other) {
+    var distX = Math.abs(this.x - other.x - other.w / 2);
+    var distY = Math.abs(this.y - other.y - other.h / 2);
+
+    if (distX > (other.w / 2 + this.radius)) {
+        return false;
+    }
+    if (distY > (other.h / 2 + this.radius)) {
+        return false;
+    }
+
+    if (distX <= (other.w / 2)) {
+        return true;
+    }
+    if (distY <= (other.h / 2)) {
+        return true;
+    }
+
+    var dx = distX - other.w / 2;
+    var dy = distY - other.h / 2;
+    return (dx * dx + dy * dy <= (this.r * this.r));
+}
+
+Circle.prototype.collideLeft = function () {
+    return (this.x - this.radius) < 0;
+};
+
+Circle.prototype.collideRight = function () {
+    return (this.x + this.radius) > 800;
+};
+
+Circle.prototype.collideTop = function () {
+    return (this.y - this.radius) < 0;
+};
+
+Circle.prototype.collideBottom = function () {
+    return (this.y + this.radius) > 800;
+};
+
+Circle.prototype.update = function () {
+    Entity.prototype.update.call(this);
+
+    this.x += this.velocity.x * this.game.clockTick;
+    this.y += this.velocity.y * this.game.clockTick;
+
+    if (this.collideLeft() || this.collideRight()) {
+        this.velocity.x = -this.velocity.x * friction;
+        if (this.collideLeft()) this.x = this.radius;
+        if (this.collideRight()) this.x = 800 - this.radius;
+        this.x += this.velocity.x * this.game.clockTick;
+        this.y += this.velocity.y * this.game.clockTick;
+    }
+
+    if (this.collideTop() || this.collideBottom()) {
+        this.velocity.y = -this.velocity.y * friction;
+        if (this.collideTop()) this.y = this.radius;
+        if (this.collideBottom()) this.y = 800 - this.radius;
+        this.x += this.velocity.x * this.game.clockTick;
+        this.y += this.velocity.y * this.game.clockTick;
+    }
+
+    for (var i = 0; i < this.game.entities.length; i++) {
+        var ent = this.game.entities[i];
+        if ((ent != this && this.collide(ent)) || (ent != this && this.rectCircleColliding(ent))) {
+            console.log("hello");
+            console.log("circle: ", (ent != this && this.collide(ent)))
+            console.log("Rectangle", ent != this && this.rectCircleColliding(ent));
+            var temp = { x: this.velocity.x, y: this.velocity.y };
+
+            var dist = distance(this, ent);
+            var delta = this.radius + ent.radius - dist;
+            var difX = (this.x - ent.x)/dist;
+            var difY = (this.y - ent.y)/dist;
+
+            this.x += difX * delta / 2;
+            this.y += difY * delta / 2;
+            ent.x -= difX * delta / 2;
+            ent.y -= difY * delta / 2;
+
+            this.velocity.x = ent.velocity.x * friction;
+            this.velocity.y = ent.velocity.y * friction;
+            ent.velocity.x = temp.x * friction;
+            ent.velocity.y = temp.y * friction;
+            this.x += this.velocity.x * this.game.clockTick;
+            this.y += this.velocity.y * this.game.clockTick;
+            ent.x += ent.velocity.x * this.game.clockTick;
+            ent.y += ent.velocity.y * this.game.clockTick;
+            if (this.it) {
+                this.setNotIt();
+                ent.setIt();
+                ent.radius += 2;
+            }
+            else if (ent.it) {
+                this.setIt();
+                ent.setNotIt();
+                ent.radius -= 1;
+            }
+        }
+
+        if ((ent != this && this.collide({ x: ent.x, y: ent.y, radius: this.visualRadius })) || (ent != this && this.rectCircleColliding({x: ent.x, y: ent.y}))) {
+            //console.log("hello");
+            console.log("circle: ", (ent != this && this.collide({ x: ent.x, y: ent.y, radius: this.visualRadius })))
+            console.log("Rectangle", ent != this && this.rectCircleColliding({x: ent.x, y: ent.y}));
+            var dist = distance(this, ent);
+            
+			//red
+			if (this.it && dist > this.radius + ent.radius + 10) {
+                var difX = (ent.x - this.x)/dist;
+                var difY = (ent.y - this.y)/dist;
+                this.velocity.x += difX * acceleration / (dist*dist);
+                this.velocity.y += difY * acceleration / (dist * dist);
+                var speed = Math.sqrt(this.velocity.x*this.velocity.x + this.velocity.y*this.velocity.y);
+                if (speed > maxSpeed) {
+                    var ratio = maxSpeed / speed;
+                    this.velocity.x *= ratio;
+                    this.velocity.y *= ratio;
+                }
+            }
+			//white
+            if (ent.it && dist > this.radius + ent.radius) {
+                var difX = (ent.x - this.x) / dist;
+                var difY = (ent.y - this.y) / dist;
+                this.velocity.x -= difX * acceleration / (dist * dist);
+                this.velocity.y -= difY * acceleration / (dist * dist);
+                var speed = Math.sqrt(this.velocity.x * this.velocity.x + this.velocity.y * this.velocity.y);
+                if (speed > maxSpeed) {
+                    var ratio = maxSpeed / speed;
+                    this.velocity.x *= ratio;
+                    this.velocity.y *= ratio;
+                }
+            }
+        }
+    }
+
+
+    this.velocity.x -= (1 - friction) * this.game.clockTick * this.velocity.x;
+    this.velocity.y -= (1 - friction) * this.game.clockTick * this.velocity.y;
+};
+
+Circle.prototype.draw = function (ctx) {
+    ctx.beginPath();
+    ctx.fillStyle = this.colors[this.color];
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fill();
+    ctx.closePath();
+
+};
+
+
+
+// the "main" code begins here
+function Rectangle(game, startX, startY) {
+	this.x = startX;
+	this.y = startY;
+	this.width = Math.floor(Math.random() * 100 + 10);
+	this.height = Math.floor(Math.random() * 100 + 10);
+	this.colors = ["White", "Red", "Green", "Blue"];
+	Entity.call(this, game, this.x, this.y);
+}
+
+
+Rectangle.prototype = new Entity();
+Rectangle.prototype.constructor = Rectangle;
+
+Rectangle.prototype.update = function () {
+	
+};
+
+Rectangle.prototype.draw = function (ctx) {
+    ctx.beginPath();
+    
+	ctx.lineWidth = "6";
+	ctx.strokeStyle = this.colors[Math.floor(Math.random() * 3)];
+	
+    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.stroke();
+
+};
+
+
+
+var friction = 1;
+var acceleration = 1000000;
+var maxSpeed = 200;
+
+var ASSET_MANAGER = new AssetManager();
+
+ASSET_MANAGER.queueDownload("./img/960px-Blank_Go_board.png");
+ASSET_MANAGER.queueDownload("./img/black.png");
+ASSET_MANAGER.queueDownload("./img/white.png");
+
+ASSET_MANAGER.downloadAll(function () {
+    console.log("starting up da sheild");
+    var canvas = document.getElementById('gameWorld');
+    var ctx = canvas.getContext('2d');
+
+    
+    var gameEngine = new GameEngine();
+    var circle = new Circle(gameEngine);
+	var rect = new Rectangle(gameEngine, Math.floor(Math.random() * 650) ,Math.floor(Math.random() * 650));
+	
+    circle.setIt();
+    gameEngine.addEntity(circle);
+	gameEngine.addEntity(rect);
+	
+    for (var i = 0; i < 12; i++) {
+        circle = new Circle(gameEngine);
+        gameEngine.addEntity(circle);
+    }
+    
+	for (var i = 0; i < 5; i++) {
+		rect = new Rectangle(gameEngine, Math.floor(Math.random() * 650), Math.floor(Math.random() * 650));
+		gameEngine.addEntity(rect);
+	}
+	
+    gameEngine.init(ctx);
+    gameEngine.start();
+});
